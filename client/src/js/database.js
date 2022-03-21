333b86c84710b0fb21a4adc13ed07b5edae9ff86
Ramil Rakhamimov <rakhamimovramil@gmail.com>
2022-03-21 16:56:15 -0400

    structure and functionality

---
+++ client/src/js/database.js
@@ -0,0 +1,48 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Post to DB')
+  
+  const indexedDB = await openDB('jate', 1);
+
+  const tx = indexedDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({id: 1, value: content});
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result)
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('Got from DB')
+  
+  const indexedDB = await openDB('jate', 1);
+
+  const tx = indexedDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value)
+  return result?.value;
+};
+
+initdb();
